2baac8f02178f20e7d7d92409338df1c1c829a27
Scott Miller <scott@millergeek.xyz>
2018-04-18 17:34:45 -0400

    get linter and tests passing to start

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
@@ -23,4 +23,4 @@ module.exports = {
     '!src/main.js',
     '!**/node_modules/**'
   ]
-}
+};
