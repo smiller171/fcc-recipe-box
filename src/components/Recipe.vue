728551b1038eab23394d0f7271fc5a2923628667
Scott Miller <scott@millergeek.xyz>
2018-04-20 16:18:49 -0400

    Add Recipe header slot

---
+++ src/components/Recipe.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot
+      :value="header"
+      name="header"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: String,
+      default: 'header'
+    }
+  }
+}
+</script>
--- test/unit/specs/Recipe.spec.js
+++ test/unit/specs/Recipe.spec.js
@@ -2,5 +2,32 @@ import Recipe from '@/components/Recipe'
 import { shallow } from '@millergeek/test-utils'
 
 describe('Recipe.vue', () => {
+  const cmp = shallow(Recipe, {
+    scopedSlots: {
+      header: '<h2 slot-scope="header">{{header.value}}</h2>'
+    }
+  })
 
+  it('should be wraped in a div', () => {
+    const expected = expect.stringMatching(/^<div.*>.*<\/div>$/)
+    expect(cmp.html()).toEqual(expected)
+  })
+  it('should have a slot called "header"', () => {
+    const expected = expect.stringMatching(/<h2.*>.*<\/h2>/)
+    expect(cmp.html()).toEqual(expected)
+  })
+  it('should have a prop called "header"', () => {
+    expect(cmp.vm.header).toBeDefined()
+  })
+  it('should have correct value for "header" prop', () => {
+    const header = 'header-test'
+    cmp.setProps({header})
+    expect(cmp.vm.header).toEqual(header)
+  })
+  it('shoud have a scoped header slot that passes the value of the header', () => {
+    const header = 'header-test'
+    const headerRegEx = new RegExp(`<h2.*>${header}</h2>`)
+    const expected = expect.stringMatching(headerRegEx)
+    expect(cmp.html()).toEqual(expected)
+  })
 })
